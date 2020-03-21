35088734e96be27cf8aacdc0fec5aa1feb45d9ae
Shahar Stahi <shaharstahi@gmail.com>
2020-03-21 19:19:59 +0200

    Added main

--- src/main/java/org/example/MultiplyVectors.java
+++ src/main/java/org/example/MultiplyVectors.java
@@ -1,16 +1,48 @@
 package org.example;
 
 import java.util.Random;
+import java.util.Vector;
 
 public class MultiplyVectors {
 
     public static void main(String[] args) {
 
-        int n = 5, N = 5;
-        Integer[] vector1 = setVector(n), vector2 = setVector(n);
+        int vectorSize = Integer.parseInt(args[0]);
+        int numOfThreads = Integer.parseInt(args[1]);
 
-        
+        if (numOfThreads > vectorSize)
+            numOfThreads = vectorSize;
 
+        Integer[] vector1 = setVector(vectorSize), vector2 = setVector(vectorSize);
+        ThreadCalculator[] threadCalculators = new ThreadCalculator[numOfThreads];
+        Vector<Long> results = new Vector<>();
+
+        int eachThreadJob = vectorSize / numOfThreads;
+        int currIndex = 0;
+        for (int i = 0; i < numOfThreads - 1; ++i, currIndex += eachThreadJob)
+            threadCalculators[i] = new ThreadCalculator(vector1, vector2, results, currIndex, currIndex + eachThreadJob);
+
+        threadCalculators[numOfThreads - 1] = new ThreadCalculator(vector1, vector2, results, currIndex, vectorSize);
+
+        for (ThreadCalculator t : threadCalculators)
+            t.start();
+
+        for (ThreadCalculator t : threadCalculators)
+        {
+            try
+            {
+                t.join();
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        Long sum = 0L;
+        for (Long result : results)
+            sum += result;
+
+        System.out.println(sum);
     }
 
     public static Integer[] setVector(int size) {
--- src/main/java/org/example/ThreadCalculator.java
+++ src/main/java/org/example/ThreadCalculator.java
@@ -6,12 +6,12 @@ public class ThreadCalculator extends Thread {
 
     private Integer[] vector1;
     private Integer[] vector2;
-    private Vector<Integer> resultVector;
+    private Vector<Long> resultVector;
     private int startIndex;
     private int endIndex;
 
 
-    public ThreadCalculator(Integer[] vector1, Integer[] vector2, Vector<Integer> resultVector, int startIndex, int endIndex) {
+    public ThreadCalculator(Integer[] vector1, Integer[] vector2, Vector<Long> resultVector, int startIndex, int endIndex) {
         this.vector1 = vector1;
         this.vector2 = vector2;
         this.resultVector = resultVector;
@@ -20,10 +20,11 @@ public class ThreadCalculator extends Thread {
     }
     @Override
     public void run() {
-        int sum = 0;
+        long sum = 0;
         for (int i = startIndex; i < endIndex; ++i)
             sum += vector1[i] * vector2[i];
 
-        resultVector.add(sum); // Vector is thread-safe, so no need for synchronisation
+        System.out.println("Adding " + sum);
+        resultVector.add(sum); // Vectors are thread-safe - no need for synchronisation
     }
 }
